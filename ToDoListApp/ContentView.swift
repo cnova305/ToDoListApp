77083928d60b320aefa504317e245d58d63a7ef7
Christopher Juwao <njuwao@icloud.com>
2020-11-20 15:46:13 +0200

    Initial Commit

---
+++ ToDoListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoListApp
+//
+//  Created by Christopher Juwao on 2020/11/20.
+//  Copyright © 2020 Christopher Juwao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
